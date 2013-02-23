bea20551c2982c64dbe20ea88766cf7f93b9f6eb
Brandon Duncan <molltanem2000@hotmail.com>
2013-02-22 20:16:14 -0800

    asdasdasdasd

--- include/introscene.hpp
+++ include/introscene.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "scene.hpp"
+#include "animation.hpp"
 
 namespace tsukino
 {
@@ -10,12 +11,15 @@ class IntroScene : public Scene
 {
 public:
     IntroScene();
+    ~IntroScene();
 
     void init();
     void update(float dt);
     void draw(sf::RenderTarget& target);
     void exit();
 private:
+    AnimData* backgroundAnimData;
+    Animation* backgroundAnim;
 };
 
 }
--- src/introscene.cpp
+++ src/introscene.cpp
@@ -5,7 +5,14 @@ namespace tsukino
 
 IntroScene::IntroScene()
 {
+ backgroundAnimData = new AnimData("assets/bird");
+ backgroundAnim = new Animation(*backgroundAnimData);
+}
 
+IntroScene::~IntroScene()
+{
+ delete backgroundAnimData;
+ delete backgroundAnim;
 }
 
 void IntroScene::init()
@@ -18,6 +25,8 @@ void IntroScene::update(float dt)
 
 void IntroScene::draw(sf::RenderTarget& target)
 {
+    target.Clear();
+    backgroundAnim->draw(target);
 }
 
 void IntroScene::exit()
