f644c4d9b5d553df532a7d49f40043f214aceffb
Nicolas Guillemot <nlguillemot@gmail.com>
2013-02-22 20:48:47 -0800

    work on menuscene

--- src/menuscene.cpp
+++ src/menuscene.cpp
@@ -1,14 +1,43 @@
 #include "menuscene.hpp"
 
+#include <fstream>
+#include <sstream>
+
 namespace tsukino
 {
 
 MenuScene::MenuScene()
 {
+    slotAnimData = new AnimData("assets/slot");
+
+    std::ifstream ifs("menu/mainmenu.menu");
+    std::string line;
+
+    while (std::getline(ifs,line))
+    {
+        if (line.substr(0,4) == "slot")
+        {
+            std::stringstream ss;
+            ss << line.substr(4);
+
+            sf::Vector2f v;
+            ss >> v.x;
+            ss >> v.y;
+
+            Animation* slot = new Animation(*slotAnimData);
+            slot->set_position(v);
+            slotAnims.push_back(slot);
+        }
+    }
 }
 
 MenuScene::~MenuScene()
 {
+    delete slotAnimData;
+    for (Animation* a : slotAnims)
+    {
+        delete a;
+    }
 }
 
 void MenuScene::handle_event(const sf::Event &e)
@@ -18,10 +47,20 @@ void MenuScene::handle_event(const sf::Event &e)
 
 void MenuScene::update(float dt)
 {
+    for (Animation* a : slotAnims)
+    {
+        a->update(dt);
+    }
 }
 
 void MenuScene::draw(sf::RenderTarget &target)
 {
+    target.Clear();
+
+    for (Animation* a : slotAnims)
+    {
+        a->draw(target);
+    }
 }
 
 }
